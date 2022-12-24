9973199d7cb02ab83fc64c1671a1abf712234b30
Ariel <xarielah@gmail.com>
2022-12-24 02:06:43 +0200

    Blur to Navbar BG and fixed position

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import SearchProjects from '../components/pages/home/search-projects';
 export default function Home() {
     return (
         <section>
-            <div className='text-center my-24 flex flex-col space-y-6 items-center justify-center'>
+            <div className='text-center flex flex-col space-y-6 min-h-[100vh] items-center justify-center'>
                 <h1 className='main-title'>Magic happens here.</h1>
                 <p className='text-xl'>
                     Hi, My name is Ariel &amp; I'm a Web-Developer based in{' '}
