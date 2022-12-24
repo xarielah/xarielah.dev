c7669b2f0a8f7bd233593c28c950825d3a8cf5c5
Ariel <xarielah@gmail.com>
2022-12-24 10:50:56 +0200

    fix: z-index to allow navigation over everything

--- components/header/navigation.tsx
+++ components/header/navigation.tsx
@@ -1,52 +1,52 @@
-import Link from 'next/link';
-import RestictWidth from '../layout/restrict-width';
+import Link from "next/link";
+import RestictWidth from "../layout/restrict-width";
 
 type Route = {
-    path: string;
-    name: string;
-    target?: string;
+  path: string;
+  name: string;
+  target?: string;
 };
 
 const Navigation = () => {
-    const routes: Route[] = [
-        {
-            path: '/',
-            name: 'Home',
-        },
-        {
-            path: '/about',
-            name: 'About',
-        },
-        {
-            path: 'https://github.com/xarielah',
-            name: '@GitHub',
-            target: '_blank',
-        },
-    ];
+  const routes: Route[] = [
+    {
+      path: "/",
+      name: "Home",
+    },
+    {
+      path: "/about",
+      name: "About",
+    },
+    {
+      path: "https://github.com/xarielah",
+      name: "@GitHub",
+      target: "_blank",
+    },
+  ];
 
-    return (
-        <RestictWidth className='fixed w-full top-0 mx-auto'>
-            <nav className='text-slate-700 backdrop-blur-sm py-5 px-10 dark:bg-stone-700 dark:text-white rounded-xl'>
-                <div className='flex justify-center sm:justify-between mx-auto items-center sm:space-x-10'>
-                    <div className='text-xl font-bold hidden sm:block'>
-                        <Link href='/'>xarielah.dev</Link>
-                    </div>
-                    <ul className='flex justify-center font-medium'>
-                        {routes.map((route) => (
-                            <li
-                                key={route.name}
-                                className='hover:text-slate-500 mx-2 ease-in-out duration-300 rounded-xl py-1 px-3'
-                            >
-                                <Link href={route.path} target={route.target}>
-                                    {route.name}
-                                </Link>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </nav>
-        </RestictWidth>
-    );
+  return (
+    <RestictWidth className="fixed w-full top-0 mx-auto z-[99]">
+      <nav className="text-slate-700 backdrop-blur-sm py-5 px-10 dark:bg-stone-700 dark:text-white rounded-xl">
+        <div className="flex justify-center sm:justify-between mx-auto items-center sm:space-x-10">
+          <div className="text-xl font-bold hidden sm:block">
+            <Link href="/">xarielah.dev</Link>
+          </div>
+          <ul className="flex justify-center font-medium">
+            {routes.map((route) => (
+              <li
+                key={route.name}
+                className="hover:text-slate-500 mx-2 ease-in-out duration-300 rounded-xl py-1 px-3"
+              >
+                <Link href={route.path} target={route.target}>
+                  {route.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </RestictWidth>
+  );
 };
 
 export default Navigation;
