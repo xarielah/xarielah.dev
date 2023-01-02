40d169a38b5f721e158d8554b86f2c5d403a4028
arielahr <arielah1997@gmail.com>
2023-01-02 14:24:02 +0200

    Removed bad import

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import AnimationLayout from '../../components/layout/animation-layout';
-import RestictWidth from '../../components/layout/restrict-width';
 import Information from '../../components/pages/about/information';
-import Technologies from '../../components/pages/about/technologies';
 
 const About = () => {
   return (
